01694cdc788493ae08b7f0cae12c6cd56b6efdd5
Alexandre <a.blanc100@laposte.net>
2023-09-20 17:05:02 +0200

    initial commit

---
+++ src/main/java/fr/mns/jee/erasmusnetwork/ErasmusNetworkApplication.java
@@ -0,0 +1,13 @@
+package fr.mns.jee.erasmusnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasmusNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErasmusNetworkApplication.class, args);
+	}
+
+}
