e4f587a5e51bb608bb15222b3dce695f7360ff27
Haorsis <cadetgauthier3@gmail.com>
2023-09-26 16:00:39 +0200

    working on comments

--- src/main/java/fr/mns/jee/erasmusnetwork/like/controller/LikeController.java
+++ src/main/java/fr/mns/jee/erasmusnetwork/like/controller/LikeController.java
@@ -1,18 +1,16 @@
 package fr.mns.jee.erasmusnetwork.like.controller;
 
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import fr.mns.jee.erasmusnetwork.like.LikeRestClient;
-import fr.mns.jee.erasmusnetwork.like.struct.LikedCommentStruct;
 
 @Controller
 @RequestMapping("/stats")
@@ -35,14 +33,14 @@ public class LikeController {
 	}
 
 	@GetMapping("/comment")
-	public ModelAndView getCommentStats() {
-		ModelAndView mv = new ModelAndView("like/commentStats");
-		mv.addObject("comments", client.findAllComments());
-		return mv;
+	public String getCommentStats(Model model) {
+		model.addAttribute("comments", client.findAllComments());
+		return "like/stats";
 	}
 	
 	@PostMapping("/commentByUser")
-	public List<LikedCommentStruct> getCommentsByUserId(@RequestParam int userId) {
-		return client.findById(userId);
+	public String getCommentsByUserId(Model model, @ModelAttribute("userId") int userId) {
+		model.addAttribute("userLikedComments", client.findById(userId));
+		return "like/stats";
 	}
 }
