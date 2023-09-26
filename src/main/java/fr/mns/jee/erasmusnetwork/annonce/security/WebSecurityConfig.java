582830d6ca600826cc939983773181f0492d2a61
KREMER Hugo <33060853+Metzill@users.noreply.github.com>
2023-09-26 14:37:11 +0200

    fix(security): moved back WebSecurityConfig.java to annonce folder

--- src/main/java/fr/mns/jee/erasmusnetwork/annonce/security/WebSecurityConfig.java
+++ src/main/java/fr/mns/jee/erasmusnetwork/annonce/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package security;
+package fr.mns.jee.erasmusnetwork.annonce.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
