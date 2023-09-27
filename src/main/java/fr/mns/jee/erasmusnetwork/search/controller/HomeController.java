24da84a34c79456f02c011843cc6ef04a339af81
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2023-09-27 15:38:44 +0200

    fix: fix for route index

--- src/main/java/fr/mns/jee/erasmusnetwork/search/controller/HomeController.java
+++ src/main/java/fr/mns/jee/erasmusnetwork/search/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "search/index";
+        return "index";
     }
     @GetMapping("/search")
     public String searchUser() {
