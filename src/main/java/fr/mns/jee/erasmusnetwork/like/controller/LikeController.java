2cc0114e368207dc1a6ab8a8679b0d99bf762251
Haorsis <cadetgauthier3@gmail.com>
2023-09-27 15:01:00 +0200

    controllers

--- src/main/java/fr/mns/jee/erasmusnetwork/like/controller/LikeController.java
+++ src/main/java/fr/mns/jee/erasmusnetwork/like/controller/LikeController.java
@@ -3,31 +3,33 @@ package fr.mns.jee.erasmusnetwork.like.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.mns.jee.erasmusnetwork.like.LikeRestClient;
 
 @Controller
+@RequestMapping("/like")
 public class LikeController {
 
 	@Autowired
 	private LikeRestClient client;
 
-	@PostMapping("/like/comment")
+	@PostMapping("/comment")
 	public String likeComment(@RequestParam(name = "likeId") int likeId) {
 		client.unlikeComment(likeId);
 
 		// TODO redirection
 		return "";
 	}
-	@PostMapping("/like/post")
+	@PostMapping("/post")
 	public String likePost(@RequestParam(name = "likeId") int likeId) {
 		client.unlikePost(likeId);
 
 		// TODO redirection
 		return "";
 	}
-	@PostMapping("/like/user")
+	@PostMapping("/user")
 	public String likeUser(@RequestParam(name = "likeId") int likeId) {
 		client.unlikeUser(likeId);
 
--- src/main/java/fr/mns/jee/erasmusnetwork/like/controller/UnlikeController.java
+++ src/main/java/fr/mns/jee/erasmusnetwork/like/controller/UnlikeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.mns.jee.erasmusnetwork.like.LikeRestClient;
 import fr.mns.jee.erasmusnetwork.like.struct.LikedCommentStruct;
@@ -11,25 +12,26 @@ import fr.mns.jee.erasmusnetwork.like.struct.LikedPostStruct;
 import fr.mns.jee.erasmusnetwork.like.struct.LikedUserStruct;
 
 @Controller
+@RequestMapping("/unlike")
 public class UnlikeController {
 	@Autowired
 	private LikeRestClient client;
 
-	@PostMapping("/unlike/comment")
+	@PostMapping("/comment")
 	public String likeComment(@RequestBody LikedCommentStruct commentStruct) {
 		client.likeComment(commentStruct);
 
 		// TODO redirection
 		return "";
 	}
-	@PostMapping("/unlike/post")
+	@PostMapping("/post")
 	public String likePost(@RequestBody LikedPostStruct postStruct) {
 		client.likePost(postStruct);
 
 		// TODO redirection
 		return "";
 	}
-	@PostMapping("/unlike/user")
+	@PostMapping("/user")
 	public String likeUser(@RequestBody LikedUserStruct userStruct) {
 		client.likeUser(userStruct);
 
